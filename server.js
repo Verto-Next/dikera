6e8d3c9d53e1a576f0828b21e1bf316390c245d3
verto-next <tayfurersin@gmail.com>
2026-04-24 14:01:22 +0300

    node.js change

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  express.static(path.join(__dirname), {
+    extensions: ["html"],
+    index: "index.html",
+  })
+);
+
+// Clean URL routing: /hakkimizda -> hakkimizda.html
+app.get("*", (req, res, next) => {
+  const filePath = path.join(__dirname, req.path + ".html");
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      next();
+    }
+  });
+});
+
+// 404 fallback
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`DikEra website running on port ${PORT}`);
+});
